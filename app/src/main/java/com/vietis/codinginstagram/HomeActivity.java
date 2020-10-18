6058b2123e5f6259c6d0cfb5eb99aa577454923a
Vuong Van An <vananbk99@gmail.com>
2020-10-18 21:59:41 +0700

    Customizing the BottomNavigationView

--- app/src/main/java/com/vietis/codinginstagram/HomeActivity.java
+++ app/src/main/java/com/vietis/codinginstagram/HomeActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.vietis.codinginstagram.utils.BottomNavigationViewHelper;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        setupNavigationView();
+    }
+
+    /* Bottom Navigation View setup*/
+    private void setupNavigationView() {
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
     }
 }
---
+++ app/src/main/java/com/vietis/codinginstagram/utils/BottomNavigationViewHelper.java
@@ -0,0 +1,14 @@
+package com.vietis.codinginstagram.utils;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx) {
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+
+}
