00caa0a9a5e48ad0414596e45e29ab7f7c4d93ff
Vuong Van An <vananbk99@gmail.com>
2020-10-19 15:08:45 +0700

    Profile Toolbar and Menu

--- app/src/main/java/com/vietis/codinginstagram/profile/ProfileActivity.java
+++ app/src/main/java/com/vietis/codinginstagram/profile/ProfileActivity.java
@@ -1,12 +1,12 @@
 package com.vietis.codinginstagram.profile;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.vietis.codinginstagram.R;
@@ -14,15 +14,31 @@ import com.vietis.codinginstagram.utils.BottomNavigationViewHelper;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private Context mContext = ProfileActivity.this;
     private static final int ACTIVITY_NUM = 4;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
 
         setupNavigationView();
+        setupToolBar();
+    }
+
+    private void setupToolBar() {
+        Toolbar toolbar = findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.profileMenu:
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     /* Bottom Navigation View setup*/
@@ -36,4 +52,9 @@ public class ProfileActivity extends AppCompatActivity {
         menuItem.setChecked(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
 }
