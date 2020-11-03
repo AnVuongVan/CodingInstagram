60a5b488f30281796e50a6f80c96cab2c03ff2fd
Vuong Van An <vananbk99@gmail.com>
2020-11-03 10:36:34 +0700

    Universal Image Loader Config

--- app/src/main/java/com/vietis/codinginstagram/profile/EditProfileFragment.java
+++ app/src/main/java/com/vietis/codinginstagram/profile/EditProfileFragment.java
@@ -4,20 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.vietis.codinginstagram.R;
+import com.vietis.codinginstagram.utils.UniversalImageLoader;
 
 public class EditProfileFragment extends Fragment {
+    private ImageView mProfilePhoto;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        mProfilePhoto = view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+        setProfileImage();
 
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage() {
+        String imgUrl = "https://www.androidcentral.com/sites/androidcentral.com/files/styles/xlarge/public/article_images/2016/08/ac-lloyd.jpg?itok=bb72IeLf";
+        UniversalImageLoader.setImage(imgUrl, mProfilePhoto, null, "https://");
     }
 }
